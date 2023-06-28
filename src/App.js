480d127601bca4115e7dc36648e0a708b101fa1e
Danny Rodriguez <danny007111@hotmail.com>
2023-06-28 12:57:01 -0500

    Changed app.js function to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
@@ -9,40 +9,39 @@ import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 
 
-function App() {
-  const particlesInit = useCallback(main => {
-      loadFull(main);
-  }, [])
+// function App() {
+//   const particlesInit = useCallback(main => {
+//       loadFull(main);
+//   }, [])
 
-  return (
-      <div className="App">
-          <Particles className='particles' options={particlesOptions} init={particlesInit}/>
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm />
-          {/* <FaceRecognition /> */}
-      </div>
-  );
+//   return (
+//       <div className="App">
+//           <Particles className='particles' options={particlesOptions} init={particlesInit}/>
+//           <Navigation />
+//           <Logo />
+//           <Rank />
+//           <ImageLinkForm />
+//           {/* <FaceRecognition /> */}
+//       </div>
+//   );
+// }
+// export default App;
+
+
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <Particles className='particles' options={particlesOptions} init={loadFull}/>      
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+    </div>
+    );
+  }
 }
 
 export default App;
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//     <div className="App">
-      
-//       <Navigation />
-//       <Logo />
-//       <Rank />
-//       <ImageLinkForm />
-//       {/* <FaceRecognition /> */}
-//     </div>
-//     );
-//   }
-// }
-
-// export default App;
