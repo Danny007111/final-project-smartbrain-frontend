34f5cd028fb8222149d285d7affeb50d8522ca8e
Danny Rodriguez <danny007111@hotmail.com>
2023-09-11 23:23:14 -0500

    editing server.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {        
-            fetch('dpg-cjvngdh5mpss7395g650-a.oregon-postgres.render.com/register', {
+            fetch('https://smartbrain-api-srq6.onrender.com/register', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
