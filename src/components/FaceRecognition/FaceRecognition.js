8c5c6a89590171c34ae05862d052b0a2d263de53
Danny Rodriguez <danny007111@hotmail.com>
2023-09-12 21:27:03 -0500

    Editing imagelinkform

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -7,7 +7,7 @@ const FaceRecognition = ({imageUrl, box}) => {
         return (
             <div className="center ma">
                 <div className='absolute mt2'>
-                    <img id="inputImage" src={imageUrl} alt="Pleace import proper format!" width="500px" height="auto"/>
+                    <img id="inputImage" src={imageUrl} alt="Pleace import proper format! (`Copy image address`)" width="500px" height="auto"/>
                     <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
                     </div>
                 </div>
